b624afa30e09baae2b5a276cf49903bb0dfd2973
SammyoRoy <sammyo.roychowdhury@gmail.com>
2023-12-19 12:05:00 -0800

    Set up project

---
+++ Phone.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TextureButton_pressed():
+	print("Hello, Vaul Theordofalaxiassias here, I just had a couple questions...");
