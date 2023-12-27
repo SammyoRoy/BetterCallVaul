ada69faf1b304e643044642e8bc19e96695e020f
SammyoRoy <sammyo.roychowdhury@gmail.com>
2023-12-27 13:22:20 -0800

    Added PI scene

--- Screens/PI_Responses.gd
+++ Screens/PI_Responses.gd
@@ -1,26 +1,23 @@
-extends Node
+extends Label
+
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const CASE_COMPLETION_GOAL = 15;
-const JUDEGE_TEMPER_REACHED = 15;
 
-var caseCompletion = 0;
-var judgeTemper = 0;
-
-var day = 1;
-var turn = 1;
-
-# PI progress
-var PI_dialogue_key = "intro";
-var witnessesUnlocked = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass;
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	text = "I can always count on you pal, worth every cred every time. I'll talk to you soon." 
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("res://Screens/SectorMap.tscn");
